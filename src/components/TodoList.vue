bc69fd4e5d0c5065c102efcba6e91d2bd53bf789
lyae <lyaesley@gmail.com>
2022-10-18 22:16:36 +0900

    vue2 study

---
+++ src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="list-group-item text-left" @click="clickButton">
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['label'],
+  methods: {
+    clickButton() {
+      this.$emit('componentClick');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
