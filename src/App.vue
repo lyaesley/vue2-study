ed2ba1ca3fa8ea9796512147060029e189d22947
lyae <lyaesley@gmail.com>
2022-10-18 22:16:59 +0900

    vue2 study

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="col-md-6 offset-md-3">
+        <h1 class="text-center mb-4">Todo 어플리케이션</h1>
+        <input type="text" class="form-control mb-4" v-model="userInput" @keyup.enter="addNewTodo">
+
+        <div class="list-group mb-4">
+          <template v-for="(todo, index) in activeTodoList">
+            <todoList
+              v-bind:key="index"
+              :label="todo.label"
+              @componentClick="toggleTodoState(todo)"
+            />
+          </template>
+        </div>
+
+        <div class="text-right">
+          <button type="button" class="btn btn-sm" @click="changeCurrentState('active')">할 일</button>
+          <button type="button" class="btn btn-sm" @click="changeCurrentState('done')">완료</button>
+          <button type="button" class="btn btn-sm" @click="changeCurrentState('all')">전체</button>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import todoList from './components/TodoList';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      userInput: '',
+      todoList: [],
+      currentState: 'active'
+    }
+  },
+  computed: {
+    activeTodoList() {
+      return this.todoList.filter(todo => this.currentState === 'all' || todo.state === this.currentState);
+    }
+  },
+  methods: {
+    changeCurrentState(state) {
+      this.currentState = state;
+    },
+    addNewTodo() {
+      this.todoList.push({
+        label: this.userInput,
+        state: 'active'
+      });
+      this.userInput='';
+    },
+    toggleTodoState(todo) {
+      todo.state = todo.state === 'active' ? 'done' : 'active';
+    }
+  },
   components: {
-    HelloWorld
+    todoList
   }
 }
 </script>
